033465cadcc0c7d45691e624b8e06b1898fcfe4f
KALISETTY REVANTH <kalisettyrevanth@gmail.com>
2025-11-24 23:45:55 +0530

    Add files via upload

---
+++ NOV(21)TASK6_RK.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE REVAAN;
+USE REVAAN;
+
+SELECT * FROM Details;
+SELECT SUM(Profit) as total_profit FROM Details;
+SELECT COUNT(*) from Details;
+SELECT Amount from Details ORDER BY Amount DESC;
